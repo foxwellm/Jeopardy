fcccb04ca3992749cdc621f49611bed728f49ce0
Matthew Foxwell <foxwellmw@gmail.com>
2018-12-11 15:36:54 -0700

    Refactor JS1

--- JS/updateDOM.js
+++ JS/updateDOM.js
@@ -1,124 +1,93 @@
 const updateDOM = {closeBigScreen}
-const questionBoxes = document.querySelectorAll('.question-box');
-const categoryBoxes = document.querySelectorAll('.category-box');
-const answerBtns = document.querySelectorAll('.answer-btn');
-const bigScreen = document.querySelector('.big-screen');
-const closeBigScreenBtn = document.querySelector('.close-big-screen');
-const bigScreenBack = document.querySelector('.big-screen-back');
-const answerBoxes = document.querySelectorAll('.answer');
 
-
-// Delete these test buttons before deployment
-const p1Turn = document.querySelector('.p1');
-const p1End = document.querySelector('.p1E');
-const p2Turn = document.querySelector('.p2');
-const p2End = document.querySelector('.p2E');
-const p3Turn = document.querySelector('.p3');
-const p3End = document.querySelector('.p3E');
-p1Turn.addEventListener('click', yourTurn1);
-p1End.addEventListener('click', yourTurnEnd1);
-p2Turn.addEventListener('click', yourTurn2);
-p2End.addEventListener('click', yourTurnEnd2);
-p3Turn.addEventListener('click', yourTurn3);
-p3End.addEventListener('click', yourTurnEnd3);
-closeBigScreenBtn.addEventListener('click', closeBigScreen)
-
-// stop deleting 
 function createQuestionBoxListeners() {
+  const questionBoxes = document.querySelectorAll('.question-box');
   questionBoxes.forEach(function(eachQuestionBox) {
     eachQuestionBox.addEventListener('click', askQuestion);
   })
+};
+
+function playerDisplayBox(type, direction, ...boxes) {
+  let whichBoxes = [...boxes];
+  whichBoxes.forEach((box) => {
+    if(direction === 'down') {
+      document.querySelector(`.p${box}-${type}`).classList.remove('your-turn');
+    } else {
+      document.querySelector(`.p${box}-${type}`).classList.add('your-turn');
+    }
+  })
+};
+
+function DDOperations() {
+  playerDisplayBox('wager','down', 1,2,3);
+  playerDisplayBox('answer','down', 1,2,3);
+  
+  if (game.currentPlayer === 'player1') {
+    playerDisplayBox('answer', 'up', 1);
+  } else if (game.currentPlayer === 'player2') {
+    playerDisplayBox('answer', 'up', 2);
+  } else {
+    playerDisplayBox('answer', 'up', 3);
+  };
+  closeBigScreen();
+  bigScreenAskQuestion(currentDailyDouble.currentQuestion)
+}
+
+function bigScreenAskQuestion(asking) {
+  document.querySelector('.big-screen-back').innerText = asking;
+  document.querySelector('.big-screen').classList.add('ask-question');
 }
 
 
 
-const wager1button = document.querySelector('.wager-1');
-const wager2button = document.querySelector('.wager-2');
-const wager3button = document.querySelector('.wager-3');
-
-function closeAnswerBox() {
-  let boxes = [...arguments]
-  boxes.forEach((box) => {
-    document.querySelector(`.p${box}-answer`).classList.remove('your-turn');
+function createWagerBtnInputListeners() {
+  const wagerBtns = document.querySelectorAll('.wager-btn');
+  wagerBtns.forEach(function (button) {
+    button.addEventListener('click', function () {
+      currentDailyDouble.collectWager(); 
+    })
   })
-};
-
-function openAnswerBox() {
-  let boxes = [...arguments]
-  boxes.forEach((box) => {
-    document.querySelector(`.p${box}-answer`).classList.add('your-turn');
-  })
-};
-
-
-function DDOperations() {
-    wagerEnd1();
-    wagerEnd2();
-    wagerEnd3();
-    closeAnswerBox(1,2,3)
-
-    if (game.currentPlayer === 'player1') {
-      openAnswerBox(1);
-    } else if (game.currentPlayer === 'player2') {
-      openAnswerBox(2);
-    } else {
-      openAnswerBox(3);
-        };
-    
-    bigScreen.classList.remove('ask-question');
-    bigScreenBack.innerText = currentDailyDouble.currentQuestion;
-    bigScreen.classList.add('ask-question');
 }
 
 function askQuestion(event) {
   let currentQuestionID = event.target.dataset.questionid;
   if (game.currentRound === 1 && (currentQuestionID == game.DDround1)) {
     currentDailyDouble = new DailyDouble(event.target.dataset.questionid, game.manipulatedQuestionObj, game.currentRound,game.currentPlayer);
-    bigScreenBack.innerText = currentDailyDouble.currentCategory;
-    bigScreen.classList.add('ask-question');
-    wager1button.addEventListener('click', function() {
-      currentDailyDouble.collectWager(); 
-    });
-    wager2button.addEventListener('click', function() {
-      currentDailyDouble.collectWager(); 
-    });
-    wager3button.addEventListener('click', function() {
-      currentDailyDouble.collectWager(); 
-    });
-
-  if (currentDailyDouble.currentPlayer === 'player1') {
-    wager1();
-  } else if (currentDailyDouble.currentPlayer === 'player2'){
-    wager2();
+    bigScreenAskQuestion(currentDailyDouble.currentCategory)
+    createWagerBtnInputListeners();
+ 
+    if (currentDailyDouble.currentPlayer === 'player1') {
+      playerDisplayBox('wager', 'up', 1);
+    } else if (currentDailyDouble.currentPlayer === 'player2'){
+      playerDisplayBox('wager', 'up', 2);
+    } else {
+      playerDisplayBox('wager', 'up', 3);
+    } 
   } else {
-    wager3();
-  } 
-
-  }else {
     currentQuestion = new Question(event.target.dataset.questionid, game.manipulatedQuestionObj, game.currentRound);
-    bigScreenBack.innerText = currentQuestion.currentQuestion;
-    bigScreen.classList.add('ask-question');
+    bigScreenAskQuestion(currentQuestion.currentQuestion);
     whosTurn();
   }
-
+  
   const boxToDisable = event.target.closest('.question-box');
   boxToDisable.innerText = '';
   boxToDisable.removeEventListener('click', askQuestion);
 }
 
 function createPlayerInputListeners() {
+  const answerBtns = document.querySelectorAll('.answer-btn');
   answerBtns.forEach(function(button) {
     button.addEventListener('click', function() {
       const currentTextBox = event.target.previousElementSibling;
-     
+      
       if (!currentDailyDouble) {
         currentQuestion.verifyAnswer(currentTextBox.value);
         currentTextBox.value = '';
         
       }else {
-      currentDailyDouble.verifyAnswer(currentTextBox.value);
-      currentTextBox.value = '';
-      currentDailyDouble = false;
+        currentDailyDouble.verifyAnswer(currentTextBox.value);
+        currentTextBox.value = '';
+        currentDailyDouble = false;
       }
     })
   })
@@ -126,13 +95,13 @@ function createPlayerInputListeners() {
 
 //test function
 function closeBigScreen() {
-  bigScreen.classList.remove('ask-question');
-  game.tilesLeft--;
-  game.checkTilesLeft();
+  document.querySelector('.big-screen').classList.remove('ask-question');
 }
 
 
 function populateGameBoard(roundCategories,questionBoxValues) {
+  const categoryBoxes = document.querySelectorAll('.category-box');
+  const questionBoxes = document.querySelectorAll('.question-box');
   categoryBoxes.forEach(categoryBox => {
     categoryBox.innerText = roundCategories.splice(-1);
   })
@@ -142,21 +111,23 @@ function populateGameBoard(roundCategories,questionBoxValues) {
 }
 
 function whosTurn() {
-    answerBoxes.forEach((answerBox) => {
-      answerBox.classList.remove('your-turn')
-    });
+  const answerBoxes = document.querySelectorAll('.answer');
+  answerBoxes.forEach((answerBox) => {
+    answerBox.classList.remove('your-turn')
+  });
   if (game.currentPlayer === 'player1') {
-    openAnswerBox(1);
+    playerDisplayBox('answer', 'up', 1);
   } else if (game.currentPlayer === 'player2') {
-    openAnswerBox(2);
+    playerDisplayBox('answer', 'up', 2);
   } else {
-    openAnswerBox(3);
-      }
+    playerDisplayBox('answer', 'up', 3);
+  }
 }
 
 const p1Score = document.querySelector('.p1-score');
 const p2Score = document.querySelector('.p2-score');
 const p3Score = document.querySelector('.p3-score');
+
 function updatePlayerScore() {
   p1Score.innerText = player1.score;
   p2Score.innerText = player2.score;
@@ -173,73 +144,94 @@ function setPlayerNames() {
   p3Name.innerText = player3.name;
 };
 
-function bigScreenRound3() {
-  bigScreen.classList.add('ask-question');
-  bigScreen.classList.add('round-3-big-screen');
-};
+// function bigScreenRound3() {
+//   bigScreen.classList.add('ask-question');
+//   bigScreen.classList.add('round-3-big-screen');
+// };
 
 // TESTING
-const p1Wager = document.querySelector('.p1-wager');
-const p2Wager = document.querySelector('.p2-wager');
-const p3Wager = document.querySelector('.p3-wager');
-const p1WagerStart = document.querySelector('.p1W');
-const p1WagerEnd = document.querySelector('.p1WE');
-p1WagerStart.addEventListener('click', wager1);
-p1WagerEnd.addEventListener('click', wagerEnd1);
 
-function wager1() {
-  p1Wager.classList.add('your-turn');
-}
+const closeBigScreenBtn = document.querySelector('.close-big-screen');
+closeBigScreenBtn.addEventListener('click', closeBigScreenTest);
 
-function wager2() {
-  p2Wager.classList.add('your-turn');
-}
-
-function wager3() {
-  p3Wager.classList.add('your-turn');
-}
-
-function wagerEnd1() {
-  p1Wager.classList.remove('your-turn');
-}
-
-function wagerEnd2() {
-  p2Wager.classList.remove('your-turn');
-}
-
-function wagerEnd3() {
-  p3Wager.classList.remove('your-turn');
-}
-
-function yourTurn1() {
-  p1Answer.classList.add('your-turn');
-}
-
-function yourTurnEnd1() {
-  p1Answer.classList.remove('your-turn');
-}
-
-function yourTurn2() {
-  p2Answer.classList.add('your-turn');
-}
-
-function yourTurnEnd2() {
-  p2Answer.classList.remove('your-turn');
-}
-
-function yourTurn3() {
-  p3Answer.classList.add('your-turn');
-}
-
-function yourTurnEnd3() {
-  p3Answer.classList.remove('your-turn');
+function closeBigScreenTest() {
+  document.querySelector('.big-screen').classList.remove('ask-question');
+  game.checkTilesLeft();
 }
 
 
+// const p1Turn = document.querySelector('.p1');
+// const p1End = document.querySelector('.p1E');
+// const p2Turn = document.querySelector('.p2');
+// const p2End = document.querySelector('.p2E');
+// const p3Turn = document.querySelector('.p3');
+// const p3End = document.querySelector('.p3E');
+// p1Turn.addEventListener('click', yourTurn1);
+// p1End.addEventListener('click', yourTurnEnd1);
+// p2Turn.addEventListener('click', yourTurn2);
+// p2End.addEventListener('click', yourTurnEnd2);
+// p3Turn.addEventListener('click', yourTurn3);
+// p3End.addEventListener('click', yourTurnEnd3);
 
 
 
-//  currentPlayerAnswerBox() fires and rises, showing answer input
+// const p1Wager = document.querySelector('.p1-wager');
+// const p2Wager = document.querySelector('.p2-wager');
+// const p3Wager = document.querySelector('.p3-wager');
+// const p1WagerStart = document.querySelector('.p1W');
+// const p1WagerEnd = document.querySelector('.p1WE');
+// p1WagerStart.addEventListener('click', wager1);
+// p1WagerEnd.addEventListener('click', wagerEnd1);
+
+// function wager1() {
+//   p1Wager.classList.add('your-turn');
+// }
+
+// function wager2() {
+//   p2Wager.classList.add('your-turn');
+// }
+
+// function wager3() {
+//   p3Wager.classList.add('your-turn');
+// }
+
+// function wagerEnd1() {
+//   p1Wager.classList.remove('your-turn');
+// }
+
+// function wagerEnd2() {
+//   p2Wager.classList.remove('your-turn');
+// }
+
+// function wagerEnd3() {
+//   p3Wager.classList.remove('your-turn');
+// }
+
+// function yourTurn1() {
+//   p1Answer.classList.add('your-turn');
+// }
+
+// function yourTurnEnd1() {
+//   p1Answer.classList.remove('your-turn');
+// }
+
+// function yourTurn2() {
+//   p2Answer.classList.add('your-turn');
+// }
+
+// function yourTurnEnd2() {
+//   p2Answer.classList.remove('your-turn');
+// }
+
+// function yourTurn3() {
+//   p3Answer.classList.add('your-turn');
+// }
+
+// function yourTurnEnd3() {
+//   p3Answer.classList.remove('your-turn');
+// }
+
+
 
 
 
